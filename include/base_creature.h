010d1a990ce501371417231fd91f73408fb6a4ee
caskeep <caskeep@gmail.com>
2020-08-18 00:58:00 +0800

    add attack cor to creature

--- include/base_creature.h
+++ include/base_creature.h
@@ -1,5 +1,11 @@
 #pragma once
 
+#include <list>
+
+namespace compcor {
+class attack_cor_base;
+}
+
 namespace creature {
 
 class base_creature {
@@ -7,6 +13,7 @@ class base_creature {
     int hp_;
     int attack_;
     int defence_;
+    std::list<compcor::attack_cor_base*> attack_cor;
   public:
     base_creature();
     base_creature(int hp);
@@ -15,6 +22,7 @@ class base_creature {
     int hp() const { return hp_; }
     int attack() const { return attack_; }
     int defence() const { return defence_; }
+    void add_attack_cor(compcor::attack_cor_base *cor) { attack_cor.push_back(cor); }
 };
     
 } // namespace compsor
